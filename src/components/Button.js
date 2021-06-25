53b454b4f08d80c9988c16e8604a65bc11c453b7
Gab-byte4 <idontbyteg@gmail.com>
2021-06-25 14:38:23 -0300

    commit 3

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,7 +17,7 @@ const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle 
     : STYLES[0];
 
-    const checkButtonSize = SIZE.includes(buttonSize) ? buttonSize : SIZES[0]
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return (
         <Link to='/sign-up' className='btn-mobile'>

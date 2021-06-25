64845b1695f80619d1d879fd23ee7f07adebdb8b
Gab-byte4 <idontbyteg@gmail.com>
2021-06-25 12:44:55 -0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
